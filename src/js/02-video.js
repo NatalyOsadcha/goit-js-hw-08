0a7cd7a314ab6d538fdd195b56111dddecb39c6e
NatalyOsadcha <114231206+NatalyOsadcha@users.noreply.github.com>
2023-03-11 11:29:11 +0200

    added task3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,55 @@
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+localStorage.setItem("videoplayer-current-time", "timeupdate.seconds");
+console.log(localStorage.getItem("videoplayer-current-time"));
+    
+const onPlay = function ({ duration, percent, seconds }) {
+    const stopTime = evt.setCurrentTime;
+    console.log(stopTime);
+    // data is an object containing properties specific to that event
+};
+
+player.on('timeupdate', onPlay);
+
+function onPlay() {
+   localStorage.setItem("videoplayer-current-time", "timeupdate.seconds"); 
+}
+
+
+
+// {
+//     duration: 61.857
+//     percent: 0.049
+//     seconds: 3.034
+// }
+
+// timeupdate
+// Triggered as the currentTime of the video updates. 
+// It generally fires every 250ms, but it may vary depending 
+// on the browser.
+
+player.setCurrentTime(30.456).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('.feedback-form input');
+const textarea = document.querySelector('.feedback-form textarea')
+const STORAGE_KEY = 'feedback-form-state';
 
+form.addEventListener('submit', handleSubmit);
+input.addEventListener('input', throttle(handleInput, 500));
+textarea.addEventListener('input', throttle(handleInput, 500));
+populateData();
+
+function handleSubmit(evt) {
+  evt.preventDefault();
+  const {
+    elements: { email, message },
+  } = evt.currentTarget;
+  if (email.value === '' || message.value === '') {
+    alert('Please fill in all the fields!');
+  } else {
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    alert('The form has been submitted successfully!');
+  }
+}
+
+function handleInput(evt) {
+  const { email, message } = evt.target;
+  const formData = { email: email.value, message: message.value };
+
+  console.log(formData);
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateData() {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+
+  if (savedData) {
+    const { email, message } = JSON.parse(savedData);
+    form.email.value = email;
+    form.message.value = message;
+  }
+}

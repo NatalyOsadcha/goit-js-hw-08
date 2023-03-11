709fc30224b76b2540bf411598e210df7a054716
NatalyOsadcha <114231206+NatalyOsadcha@users.noreply.github.com>
2023-03-11 20:43:19 +0200

    Updated 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,73 +3,27 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
-    
-const STORAGE_KEY = "videoplayer-current-time";
+
+const STORAGE_KEY = 'videoplayer-current-time';
 
 // /////////////
- player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
- });
-    
- player.on('play', function() {
-        console.log('played the video!');
-    });
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.on('play', function () {
+  console.log('played the video!');
+});
 
 ////////////////
 
-player.on('timeupdate', onTimeupdate);
+player.on('timeupdate', throttle(onTimeupdate, 1000));
 
-function onTimeupdate() {
-    console.log('time updated')
+function onTimeupdate({ seconds }) {
+  localStorage.setItem(STORAGE_KEY, seconds);
 }
 
-//     localStorage.setItem(STORAGE_KEY, JSON.stringify(time));
+const savedTime = localStorage.getItem(STORAGE_KEY);
+// console.log(savedTime);
 
-// console.log(localStorage.getItem("videoplayer-current-time"));
-    
-// const onPlay = function ({ duration, percent, seconds }) {
-//     const stopTime = evt.setCurrentTime;
-//     console.log(stopTime);
-//     // data is an object containing properties specific to that event
-// };
-
-// player.on('timeupdate', onTimeupdate);
-
-// currentTime 
-// {
-//     duration: 61.857
-//     percent: 0.049
-//     seconds: 3.034
-// }
-
-// function onPlay() {
-//    localStorage.setItem("videoplayer-current-time", "timeupdate.seconds"); 
-// }
-
-
-
-// {
-//     duration: 61.857
-//     percent: 0.049
-//     seconds: 3.034
-// }
-
-// timeupdate
-// Triggered as the currentTime of the video updates. 
-// It generally fires every 250ms, but it may vary depending 
-// on the browser.
-
-player.setCurrentTime(30.456).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
-
-        default:
-            // some other error occurred
-            break;
-    }
-});
-
+player.setCurrentTime(savedTime || 0.001).then(function (seconds) {});

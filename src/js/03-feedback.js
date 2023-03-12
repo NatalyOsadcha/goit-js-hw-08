2f75bca829edf7535daa708e7ded480715b9d3c5
NatalyOsadcha <114231206+NatalyOsadcha@users.noreply.github.com>
2023-03-12 11:14:19 +0200

    updated task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,14 +1,11 @@
 import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
-const input = document.querySelector('.feedback-form input');
-const textarea = document.querySelector('.feedback-form textarea');
 
 const STORAGE_KEY = 'feedback-form-state';
 
 form.addEventListener('submit', handleSubmit);
 form.addEventListener('input', throttle(handleInput, 500));
-// input.addEventListener('input', handleInput, 500);
-// textarea.addEventListener('input', handleInput, 500);
+
 populateData();
 let formData = {};
 
@@ -20,6 +17,7 @@ function handleSubmit(evt) {
   if (email.value === '' || message.value === '') {
     alert('Please fill in all the fields!');
   } else {
+    console.log({ email: email.value, message: message.value });
     evt.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
     alert('The form has been submitted successfully!');
@@ -27,13 +25,9 @@ function handleSubmit(evt) {
 }
 
 function handleInput(evt) {
- 
   const key = evt.target.name;
-  formData[key]= evt.target.value;
-
-  const formData = { email: email.value, message: message.value };
-
-  console.log(formData);
+  formData[key] = evt.target.value;
+  // console.log(formData);
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
